7d9debfeeb8c5ce3fa6bd94e42b04d2b4154dcd3
RighteousDavid <daviddavis.buyers@gmail.com>
2023-12-09 10:33:08 -0500

    Update getComputerChoice to return string

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 let computerChoice;
+let playerChoice;
+
 const CHOICES = ["rock", "paper", "scissors"];
 const numChoices = CHOICES.length;
 
 console.log(numChoices);
 
 function getComputerChoice() {
-  computerChoice = Math.ceil(Math.random() * numChoices);
+  let randomNum = Math.ceil(Math.random() * numChoices);
+
+  switch (randomNum) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "sciccors";
+      break;
+  }
+
   return computerChoice;
 }
 
 console.log(getComputerChoice());
+
+function getPlayerChoice() {
+  playerChoice = prompt("Choose your weapon! Rock, paper, sciccors: ");
+  return playerChoice;
+}
